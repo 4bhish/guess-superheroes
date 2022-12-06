6253b0f61fbbf224da23719a2d9bcfc25927335d
Abhishek Hadimani <108976136+4bhish@users.noreply.github.com>
2022-12-06 11:50:51 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,60 @@
+var readlineSync = require('readline-sync'); 
+
+let score = 0;
+
+const userName = readlineSync.question("May I know your name? ");
+
+console.log("Welcome "+userName);
+console.log("___________________")
+
+
+function play(question,answer)
+  {
+    userAnswer = readlineSync.question(question);
+
+    if(userAnswer === answer)
+    {
+      console.log("You are right! ");
+      score++;
+    }
+    else
+    {
+      console.log("You are wrong! ");
+      score--;
+    }
+    console.log("Your score stands: "+score);
+    console.log("___________________")
+    
+  }
+console.log(`${userName} Answer confidently , Best of luck.`)
+const quiz = [{
+  question:"who is peter parker? ",
+  answer:"spiderman"
+},
+           {
+  question:"who is bruce Wayne? ",
+  answer:"batman"
+},
+{
+  question:"who is Clark Joseph Kent? ",
+  answer:"superman"
+},
+{
+  question:"who is Tony Stark? ",
+  answer:"ironman"
+},
+{
+  question:"who is shaktiman? ",
+  answer:"gangadhar"
+},
+{
+  question:"who is stever rogers? ",
+  answer:"captain america"
+}];
+
+for(var i=0;i<quiz.length;i++)
+  {
+    const newQuiz = quiz[i];
+    play(newQuiz.question,newQuiz.answer);
+  }
+
